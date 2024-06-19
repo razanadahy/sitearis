94564f81726c920e157413579e62a19b5b1c0e88
razanadahy <138490772+razanadahy@users.noreply.github.com>
2024-06-19 11:17:09 +0100

    modif activity

--- src/Component/TextCarousel.js
+++ src/Component/TextCarousel.js
@@ -8,20 +8,20 @@ const TextCarousel = ({title,emplacement}) => {
     const isSM = useMediaQuery({ query: '(max-width: 768px)' });
     const clas = () =>{
         if (isXL){
-            return 'w-50 top-50'
+            return 'w-50 top-10'
         }if (isLG){
-            return 'w-75 top-40'
+            return 'w-50 top-10'
         }if (isMD){
-            return 'w-75 top-30'
+            return 'w-50 top-20'
         }if(isSM){
-            return 'w-100 top-20'
+            return 'w-75 top-10'
         }
         return ''
     }
     return(
         <div className={`position-absolute ${(emplacement+1)%2===0 ? 'end-0' : 'start-0'} mx-2 rounded-4 ${clas()} pt-3 px-2 pb-1`}>
-            <h3 className="text-center color-aris">{title}</h3>
-            <p className={`${isSM ? 'fs-6' : 'fs-5'} text-white text-center`}>Nos spécialistes en informatique offrent une gamme
+            <h3 className={`text-center color-aris mb-lg-5 mb-md-2 ${isSM ? 'fs-3' : isMD ? 'fs-3' : 'fs-1'}`}>{title}</h3>
+            <p className={`${isSM ? 'fs-6' : isMD ? 'fs-5' : 'fs-3'} text-white text-center`}>Nos spécialistes en informatique offrent une gamme
                 complète de services, du développement logiciels aux
                 traitements de données en ligne et hors ligne.
             </p>

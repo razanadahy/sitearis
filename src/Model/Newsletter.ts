cad594f3cb35b81e4b28e8ecdb3b0f974a285bb4
razanadahy <138490772+razanadahy@users.noreply.github.com>
2024-06-19 16:56:52 +0100

    ajout de Newsletter

--- src/Model/Newsletter.ts
+++ src/Model/Newsletter.ts
@@ -3,4 +3,27 @@ class Newsletter {
     mail: string
     //https://backsitearis.onrender.com
 
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault(); // Empêcher le rechargement de la page
+    //
+    //     try {
+    //         const response = await fetch("https://your-endpoint-url.com/api", {
+    //             method: "POST",
+    //             headers: {
+    //                 "Content-Type": "application/json"
+    //             },
+    //             body: JSON.stringify(formData)
+    //         });
+    //
+    //         if (!response.ok) {
+    //             throw new Error("Network response was not ok");
+    //         }
+    //
+    //         const result = await response.json();
+    //         console.log("Success:", result);
+    //     } catch (error) {
+    //         console.error("Error:", error);
+    //     }
+    // };
+
 }

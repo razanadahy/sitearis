c04d44eedb12854645d08ccbc2cc8e5c0854ae0f
razanadahy <138490772+razanadahy@users.noreply.github.com>
2024-06-28 11:40:58 +0100

    modification d'ElementshowTitle

--- src/Component/ElementShowTitle.js
+++ src/Component/ElementShowTitle.js
@@ -22,8 +22,9 @@ const ElementShowTitle = ({title}) => {
     }, []);
     return(
         <>
-            <div ref={elementRef} className={`col-md-10 col-sm-11 arrow-shape bg-gradient-info-dark pt-5  pb-3  text-white ${isVisible ? 'arrow-div' : 'd-none'}`}>
-                <h2 className="text-center fs-4 text-uppercase ">{title}</h2>
+            {/*<div ref={elementRef} className={`col-md-10 col-sm-11 arrow-shape bg-gradient-info-dark pt-5  pb-3  text-white ${isVisible ? 'arrow-div' : 'd-none'}`}>*/}
+            <div ref={elementRef} className={`col-md-10 col-sm-11 arrow-shape  pt-5  pb-3  color-aris ${isVisible ? 'arrow-div' : 'd-none'}`}>
+                <h2 className="text-center fs-2 text-uppercase ">{title}</h2>
             </div>
         </>
     )

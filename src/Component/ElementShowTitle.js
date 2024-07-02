f42d25068030d50dbee301c9db03d7e8c2080116
razanadahy <138490772+razanadahy@users.noreply.github.com>
2024-07-02 11:28:29 +0100

    modification show title

--- src/Component/ElementShowTitle.js
+++ src/Component/ElementShowTitle.js
@@ -23,8 +23,32 @@ const ElementShowTitle = ({title}) => {
     return(
         <>
             {/*<div ref={elementRef} className={`col-md-10 col-sm-11 arrow-shape bg-gradient-info-dark pt-5  pb-3  text-white ${isVisible ? 'arrow-div' : 'd-none'}`}>*/}
-            <div ref={elementRef} className={`col-md-10 col-sm-11 arrow-shape  pt-5  pb-3  color-aris ${isVisible ? 'arrow-div' : 'd-none'}`}>
-                <h2 className="text-center fs-2 text-uppercase ">{title}</h2>
+            {/*<div ref={elementRef} className={`col-md-10 col-sm-11 bg-primary pt-5  pb-3  color-aris ${isVisible ? 'arrow-div' : 'd-none'}`}>*/}
+            {/*    <h2 className="text-center fs-2 text-uppercase ">{title}</h2>*/}
+            {/*</div>*/}
+            <div ref={elementRef} className={`col-md-12 col-sm-11 color-aris ${isVisible ? 'arrow-div' : 'd-none'}`}>
+                <div className="row w-100 p-0">
+                    <div className="col w-100 px-0">
+                        <div className="row m-0 p-0 w-100">
+                            <div className="col-6 px-0 pt-5 pb-3 bg-gradient-info-dark block-initial"/>
+                            <div className="col-6">
+                                <div className=" row m-0 p-0 w-100">
+                                    <div className="col-3 block-child-1 pt-5 pb-3 bg-gradient-info-dark"/>
+                                    <div className="col-3 block-child pt-5 pb-3 bg-gradient-info-dark"/>
+                                    <div className="col-3 block-child pt-5 pb-3 bg-gradient-info-dark"/>
+                                    <div className="col-3 block-child pt-5 pb-3 bg-gradient-info-dark"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col w-100 pt-3">
+                        <h2 className="text-center fs-2 text-uppercase ">{title}</h2>
+                    </div>
+                    <div className="col w-100 bg-aris">
+
+                    </div>
+
+                </div>
             </div>
         </>
     )

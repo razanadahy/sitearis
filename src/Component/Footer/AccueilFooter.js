e439c70f16487c04794247b59af056d6a1a57fba
razanadahy <andrianiavo.vit@gmail.com>
2024-10-07 11:57:38 +0100

    footer

--- src/Component/Footer/AccueilFooter.js
+++ src/Component/Footer/AccueilFooter.js
@@ -12,51 +12,80 @@ const AccueilFooter = () => {
     return (
         <>
             <IconFooter/>
-            <div className="mt-5 p-4 w-100 bg-dark-blue text-white small" style={{backgroundImage: `url('${waveFooter}')`,backgroundSize: 'cover',backgroundPosition: 'center', width:'100%', minHeight: '50vh'}}>
-                <div className="row w-100 mx-0 mt-2 mb-0 p-0">
-                    <div className="col-3">
-                        <div className="w-100 d-inline-flex align-items-center">
+            <div className="mt-5 p-0 w-100 bg-dark-blue text-white small">
+                <div className="row w-100 mx-0 mt-2 mb-0 pt-4 px-3 border-bottom">
+                    <div className="col-6">
+                        <div className="w-100 d-inline-flex align-items-center ">
                             <img src={logo} alt="logo" className="img-fluid me-3" width={40}/>
-                            <h4 className="mb-0 fs-4 fw-bold">
+                            <h4 className="mb-0 fs-2 fw-bold">
                                 <strong style={{ color: '#D10D58' }}>Aris </strong> <strong style={{ color: '#0e8de8' }}>Concept</strong>
                             </h4>
                         </div>
-                        <ul className="list-inline d-md-block d-sm-flex align-items-sm-center flex-sm-column mb-0">
-                            <li className="list-inline-item p-2 rounded-2 d-flex align-items-stretch cursor-pointer" style={{ height: '50px' }}>
-                                <div className="h-100 d-flex align-items-center flex-shrink-0">
-                                    <i className="fa fa-location-dot fa-2x"/>
+                        <div className="my-3 w-100 text-center" >
+                            <p className="" style={{fontSize: '0.875rem', lineHeight: '1.35rem'}}>
+                                Nous visons à créer des opportunités d'emploi pour les jeunes motivés, en leur permettant de développer
+                                leurs compétences aux côtés de professionnels expérimentés dans leur domaine.
+                            </p>
+                        </div>
+                        <div className="w-100 mt-3">
+                            <span className="fs-5 fw-bolder text-uppercase text-aris">Nous Contacter</span>
+                            <div className=" w-100 mt-2 p-2 d-flex justify-content-between">
+                                <div className="d-inline-flex text-jaune fw-bold">
+                                    <span className="icon me-1">+</span>
+                                    <span className="fw-bolder txt-at">Par ici</span>
                                 </div>
-                                <div className="h-100 d-flex align-items-center flex-grow-1">
-                                    <span className="ms-2">Iavoloha, Antananarivo 102</span>
+                                <div>
+                                    <span className="link-primary fw-bold">Iavoloha, Antananarivo 102</span>
                                 </div>
+                                <div>
+                                    <a  href="https://wa.me/+261385340534" target="_blank" rel="noopener noreferrer" className="ms-2 email-link link-primary">+261 38 53 405 34</a>
+                                </div>
+                                <div>
+                                    <a  href={`mailto:${'contact@aris-cc.com'}`} onClick={handleClick} className="ms-2 email-link link-primary">contact@aris-cc.com</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="mt-2 ">
+                            LinkDIN
+                        </div>
+                    </div>
+                    <div className="col-3 px-3">
+                        <h4 className="fs-3 mb-3 ps-4">Service</h4>
+                        <ul className="list-group list-group-flush bg-transparent">
+                            <li className="list-group-item bg-transparent text-white d-inline-flex justify-content-between border-0">
+                                <span className="fw-bolder p-0 m-0">Informatique et traitement de données</span>
                             </li>
-                            <li className="list-inline-item p-2 rounded-2 d-flex align-items-stretch cursor-pointer" style={{ height: '50px' }}>
-                                <div className="h-100 d-flex align-items-center flex-shrink-0" style={{height: '100%'}}>
-                                    <i className="fa fa-phone fa-2x"/>
-                                </div>
-                                <div className="h-100 d-flex align-items-center flex-grow-1">
-                                    <a  href="https://wa.me/+261385340534" target="_blank" rel="noopener noreferrer" className="ms-2 email-link">+261 38 53 405 34</a>
-                                </div>
-                            </li>
-                            <li className="list-inline-item p-2 rounded-2 d-flex align-items-stretch cursor-pointer" style={{ height: '50px' }}>
-                                <div className="h-100 d-flex align-items-center flex-shrink-0 ">
-                                    <i className="far fa-envelope fa-2x"/>
-                                </div>
-                                <div className="h-100 d-flex align-items-center flex-grow-1">
-                                    <a  href={`mailto:${'contact@aris-cc.com'}`} onClick={handleClick} className="ms-2 email-link">contact@aris-cc.com</a>
-                                </div>
-                            </li>
+                            <li className="list-group-item bg-transparent text-white border-0"><span className="fw-bolder p-0 m-0">Web et Marketing</span></li>
+                            <li className="list-group-item bg-transparent text-white border-0"><span className="fw-bolder p-0 m-0">Comptabilité</span></li>
+                            <li className="list-group-item bg-transparent text-white border-0"><span className="fw-bolder p-0 m-0">Externalisation administrative</span></li>
+                            <li className="list-group-item bg-transparent text-white mb-2 border-0"><span className="fw-bolder p-0 m-0">Ressources Humaines</span></li>
                         </ul>
+                    </div>
+                    <div className="col-3">
+                        <h4 className="ms-2 mb-3 text-primary">
+                            Newsletter
+                        </h4>
+                        <div className="">
+                            Inscrivez-vous à notre newsletter et recevez par email les dernières tendances du digital.
 
+                            <div className="email-container border-bottom mt-2 w-100 d-inline-flex justify-content-between py-2 px-1 align-items-center">
+                                <label className="email-label" htmlFor="email">
+                                    e-mail
+                                </label>
+                                <input type="email" id="email" className="email-input" />
+                                <span className="email-arrow">
+                                    <i className="fa-solid fa-arrow-right-long"/>
+                                </span>
+                            </div>
+                        </div>
                     </div>
-                    <div className="col-3">
-                        Lien
+                </div>
+                <div className="m-0 p-2 w-100 d-inline-flex justify-content-between">
+                    <div className=" text-light d-inline-flex">
+                        <span className="pb-1">© 2024 Aris Concept company - Mentions légales - Politique de confidentialité</span>
                     </div>
-                    <div className="col-3">
-                        Service
-                    </div>
-                    <div className="col-3">
-                        News Letter
+                    <div className=" text-light pb-1">
+                        <span> Tous droits réservés.</span>
                     </div>
                 </div>
             </div>
